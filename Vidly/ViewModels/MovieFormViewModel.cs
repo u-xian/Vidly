64e918960fb98bd485622ab0462dfbe672b068aa
Christian Uwakristu <uxian554@gmail.com>
2018-01-12 10:10:12 +0200

    Add new movie

---
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<GenreTypes> GenreTypes { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
