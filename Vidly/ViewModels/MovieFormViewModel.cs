65f2f1179b6c0ee6824d00fffbaed0d216db2b2b
Christian Uwakristu <uxian554@gmail.com>
2018-01-13 22:56:54 +0200

    Add Movies constructors

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -9,6 +10,49 @@ namespace Vidly.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<GenreTypes> GenreTypes { get; set; }
-        public Movie Movie { get; set; }
+
+
+        public int? Id { get; set; }
+
+        [Required]
+        [Display(Name = "Movie Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Released Date")]
+        public DateTime? ReleasedDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Number in Stock")]
+        public int? NumberStock { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public int? GenreTypesId { get; set; }
+
+
+
+        public string Title 
+        {  
+            get 
+            {
+                return (Id != 0) ? "Edit Movie" : "New Movie";
+            } 
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleasedDate = movie.ReleasedDate;
+            NumberStock = movie.NumberStock;
+            GenreTypesId = movie.GenreTypesId;
+        }
     }
 }
