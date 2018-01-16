a4c5cfdeabb0bb78641b21c5187f65a8f6d3072b
Christian Uwakristu <uxian554@gmail.com>
2018-01-16 17:39:12 +0200

    Clean customers controller

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,8 +26,8 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MemberShipType).ToList();
-            return View(customers);
+            //var customers = _context.Customers.Include(c => c.MemberShipType).ToList();
+            return View();
         }
 
         public ActionResult Details(int id)
